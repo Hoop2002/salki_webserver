054b133734efcdf84381ca3b7039fa249467fca0
Hoop2002 <hoopertygoas@gmail.com>
2024-01-08 23:05:12 +0500

    add

--- routers/auth/sign_up.py
+++ routers/auth/sign_up.py
@@ -3,6 +3,7 @@ from models import RequestUserSignUp, ResponseToken
 
 router = APIRouter()
 
+
 @router.post("/api/v1/sign-up", response_model=ResponseToken)
 async def user_sign_up(user_data: RequestUserSignUp):
     return {}
